21eebac420b2640c2cda6ef995d3d70cb098ca38
Ilan Varshavsky <ilanvarshavsky@ucmerced.edu>
2025-05-02 05:53:51 +0000

    Initial commit

---
+++ src/Application.cpp
@@ -0,0 +1,102 @@
+#include "Application.h"
+#include "Enums.h"
+
+using namespace bobcat;
+using namespace std;
+
+void Application::onCanvasMouseDown(bobcat::Widget* sender, float mx, float my) {
+    TOOL tool = toolbar->getTool();
+    Color color = colorSelector->getColor();
+
+    if (tool == PENCIL) {
+        canvas->addPoint(mx, my, color.getR(), color.getG(), color.getB(), 7);
+        canvas->redraw();
+    }
+    else if (tool == ERASER) {
+        canvas->addPoint(mx, my, 1.0, 1.0, 1.0, 14);
+        canvas->redraw();
+    }
+    else if (tool == RECTANGLE) {
+        canvas->addRectangle(mx, my, color.getR(), color.getG(), color.getB());
+        canvas->redraw();
+    }
+    else if (tool == CIRCLE) { 
+        canvas->addCircle(mx, my, color.getR(), color.getG(), color.getB());
+        canvas->redraw();
+    }
+    else if (tool == MOUSE) {
+        selectedShape = canvas->getSelectedShape(mx, my);
+    }
+
+}
+
+void Application::onCanvasDrag(bobcat::Widget* sender, float mx, float my) {
+    TOOL tool = toolbar->getTool();
+    Color color = colorSelector->getColor();
+
+    if (tool == PENCIL) {
+        canvas->addPoint(mx, my, color.getR(), color.getG(), color.getB(), 7);
+        canvas->redraw();
+    }
+    else if (tool == ERASER) {
+        canvas->addPoint(mx, my, 1.0, 1.0, 1.0, 14);
+        canvas->redraw();
+    }
+}
+
+void Application::onToolbarChange(bobcat::Widget* sender) {
+    ACTION action = toolbar->getAction();
+
+    if (action == CLEAR) {
+        canvas->clear();
+        canvas->redraw();
+    }
+    else if (action == SENDBACK){
+        canvas->sendToBack(selectedShape);
+        canvas->redraw();
+    }
+    else if (action == SENDFRONT){
+        canvas->sendToFront(selectedShape);
+        canvas->redraw();
+    }
+    else if (action == PLUS){
+        canvas->increaseSize(selectedShape);
+        canvas->redraw();
+    }
+    else if (action == MINUS){
+        canvas->decreaseSize(selectedShape);
+        canvas->redraw();
+    }
+}
+
+void Application::onColorSelectorChange(bobcat::Widget* sender) {
+    Color color = colorSelector->getColor();
+
+    if (selectedShape) {
+        cout << "Update selected shape color" << endl;
+        selectedShape->setColor(color.getR(), color.getG(), color.getB());
+        canvas->redraw();
+    }
+}
+
+Application::Application() {
+    window = new Window(25, 75, 410, 650, "Paint App");
+
+    selectedShape = nullptr;
+
+    toolbar = new Toolbar(0, 0, 60, 650);
+    canvas = new Canvas(60, 0, 350, 600);
+    colorSelector = new ColorSelector(60, 600, 350, 50);
+    colorSelector->box(FL_BORDER_BOX);
+
+    window->add(toolbar);
+    window->add(canvas);
+    window->add(colorSelector);
+
+    ON_MOUSE_DOWN(canvas, Application::onCanvasMouseDown);
+    ON_DRAG(canvas, Application::onCanvasDrag);
+    ON_CHANGE(toolbar, Application::onToolbarChange);
+    ON_CHANGE(colorSelector, Application::onColorSelectorChange);
+
+    window->show();
+}
